206f31fdbaa7d14120392f4595acdbdf58e47608
ivan <vanyarock01@gmail.com>
2019-03-15 18:56:58 +0300

    Добавлено решение СЛАУ

--- 1/main.py
+++ 1/main.py
@@ -28,10 +28,21 @@ def LU_decomposition(size, matrix):
 
         for j in range(i, size):
             s = sum(U[k][i] * L[j][k] for k in range(i))
-
             L[j][i] = (matrix[j][i] - s) / U[i][i]
 
     return L, U
+
+
+def LU_solve(size, L, U, B):
+    Z = [0 for _ in range(size)]
+    for i in range(0, size):
+        Z[i] = B[i] - sum(L[i][j] * Z[j] for j in range(i - 1))
+
+    X = [0 for _ in range(size)]
+    for i in reversed(range(size)):
+        X[i] = (1 / U[i][i]) * (Z[i] - sum(U[i][j] * X[j]
+                                           for j in range(i + 1, size)))
+    return X
 
 
 if __name__ == '__main__':
@@ -39,17 +50,22 @@ if __name__ == '__main__':
         data = json.load(json_data)
 
         size = data[0]['size']
-        matrix = data[0]['A']
-        L, U = LU_decomposition(size, matrix)
+        A = data[0]['A']
+        B = data[0]['B']
+
+        L, U = LU_decomposition(size, A)
 
         print("matrix")
-        utils.matrix_print(matrix)
+        utils.matrix_print(A)
         print("L")
         utils.matrix_print(L)
         print("L")
         utils.matrix_print(U)
         print('L*U')
+
+        X = LU_solve(size, L, U, B)
         a = np.array(L)
         b = np.array(U)
         r = np.matmul(a, b)
         utils.matrix_print(r)
+        utils.vector_print(X)
--- 1/utils.py
+++ 1/utils.py
@@ -1,6 +1,12 @@
 def matrix_print(matrix):
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
+        for j in range(len(matrix[i])):
             print('{:6.2f}'.format(float(matrix[i][j])), end=' ')
         print()
+    print()
+
+
+def vector_print(vec):
+    for i in range(len(vec)):
+        print('{:6.2f}'.format(float(vec[i])), end=' ')
     print()
