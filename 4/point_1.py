e9e72dde15055e90f3fd0491f8be50df12bce676
Naidenov Ivan <vanyarock01@gmail.com>
2019-06-01 00:38:20 +0300

    Fix methods, now correct

--- 4/point_1.py
+++ 4/point_1.py
@@ -46,7 +46,7 @@ def precise(x):
 
 
 def g(x, y, z):
-    return z * math.tan(x) - y * (math.cos(x) ** 2)
+    return -z * math.tan(x) + y * (math.cos(x) ** 2)
 
 
 def f(x, y, z):
@@ -65,23 +65,82 @@ def solve(f, p):
 
 
 def euler(g, f, p):
-    x0, y0, z, h = p["xmin"], p["y0"], p["z0"], p["h"]
+    x0, y0, z0, h = p["xmin"], p["y0"], p["z0"], p["h"]
     n = getn(p["xmin"], p["xmax"], h)
 
-    X, Y = [x0], [y0]
+    X, Y, Z = [x0], [y0], [z0]
     for k in range(n):
-        z += h * g(X[k], Y[k], z)
-
+        Y.append(Y[k] + h * f(X[k], Y[k], Z[k]))
+        Z.append(Z[k] + h * g(X[k], Y[k], Z[k]))
         X.append(X[k] + h)
-        Y.append(Y[k] + h * f(X[k], Y[k], z))
 
-    return X, Y
+    return X, Y, Z
+
+
+def runge_kutta(g, f, p):
+    degree = 4
+    a = [0, 1/2, 1/2,   1]
+    c = [1/6, 1/3, 1/3, 1/6]
+    b = [[],
+         [1/2, ],
+         [0,  1/2],
+         [0,    0, 1/2]]
+
+    err = []
+    x0, y0, z0, h = p["xmin"], p["y0"], p["z0"], p["h"]
+    n = getn(p["xmin"], p["xmax"], h)
+    X, Y, Z = [x0], [y0], [z0]
+    K, L = [0.] * degree, [0.] * degree
+    for k in range(n - 1):
+        y_, z_ = 0, 0
+        for i in range(degree):
+            sum_K = sum( b[i][j] * K[j] for j in range(i) )
+            sum_L = sum( b[i][j] * L[j] for j in range(i) )
+
+            K[i] = h * \
+                f(X[k] + a[i] * h, Y[k] + sum_K, Z[k] + sum_L)
+            L[i] = h * \
+                g(X[k] + a[i] * h, Y[k] + sum_K, Z[k] + sum_L)
+            
+            y_ += c[i] * K[i]
+            z_ += c[i] * L[i]
+        Y.append(Y[k] + y_)
+        Z.append(Z[k] + z_)
+        X.append(X[k] + h)
+        if abs(K[0] - K[1]) > 0:
+            err.append((K[1] - K[2]) / (K[0] - K[1]))
+        else:
+            err.append(0.0)
+        # print(err)
+    return X, Y, Z
+
+
+def adams(g, f, p):
+    x0, y0, z0, h = p["xmin"], p["y0"], p["z0"], p["h"]
+    n = getn(p["xmin"], p["xmax"], h)
+
+    X, Y, Z = runge_kutta(g, f, p)
+
+    for k in range(3, n):
+        y_ = 55 * f(X[k], Y[k], Z[k]) \
+            - 59 * f(X[k - 1], Y[k - 1], Z[k - 1]) \
+            + 37 * f(X[k - 2], Y[k - 2], Z[k - 2]) \
+            - 9 * f(X[k - 3], Y[k - 3], Z[k - 3])
+        z_ = 55 * g(X[k], Y[k], Z[k]) \
+            - 59 * g(X[k - 1], Y[k - 1], Z[k - 1]) \
+            + 37 * g(X[k - 2], Y[k - 2], Z[k - 2]) \
+            - 9 * g(X[k - 3], Y[k - 3], Z[k - 3])
+        Y.append(Y[k] + h / 24 * y_)
+        Z.append(Z[k] + h / 24 * z_)
+        X.append(X[k] + h)
+    return X, Y, Z
 
 
 methods = {
-    "euler": euler
+    "euler":       euler,
+    "runge_kutta": runge_kutta,
+    "adams":       adams
 }
-
 
 
 def main():
@@ -91,7 +150,7 @@ def main():
     X, Y, labels = [s[0]], [s[1]], ["precision"]
 
     for label, bar in methods.items():
-        x_, y_ = bar(g, f, params)
+        x_, y_, _ = bar(g, f, params)
         X.append(x_)
         Y.append(y_)
         labels.append(label)
